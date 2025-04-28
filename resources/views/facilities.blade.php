edb4b6259df55eba8c3593d892a3182426475992
Erhaes <rassyahafizh@gmail.com>
2025-04-28 21:39:06 +0700

    tambah lagi

--- resources/views/facilities.blade.php
+++ resources/views/facilities.blade.php
@@ -3,6 +3,94 @@
 @section('title', 'Fasilitas')
 
 @section('content')
-
-
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Fasilitas
+                </h2>
+            </div>
+            <div class="grid gap-8 lg:grid-cols-2">
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Lab. Transport.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Laboratorium
+                            Transportasi</h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Lab. Hidro.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Laboratorium
+                            Hidroteknik</h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Lab. Struktur.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Laboratorium
+                            Struktur dan Bahan Bangunan</h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Lab. Mektan.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Laboratorium
+                            Mekanika Tanah</h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Transport.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Laboratorium
+                            Pemetaan</h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Admin pendaftaran.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Ruang
+                            Administrasi</h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Ruang Rapat.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Ruang Rapat
+                        </h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Loby ruang tunggu.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Lobby/Ruang
+                            Tunggu</h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Ruang Kantor.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Ruang Kantor
+                        </h2>
+                    </a>
+                </div>
+                <div class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <a href="#">
+                        <img src="{{ asset('/images/Ruang/Work Shop.jpeg') }}"
+                            class="w-full h-75 object-cover rounded-2xl mb-5" alt="">
+                        <h2 class="text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">Ruang
+                            WorkShop</h2>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -7,24 +7,23 @@
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
             <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Berita Terkini</h2>
-            {{-- <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p> --}}
         </div> 
         <div class="grid gap-8 lg:grid-cols-2">
             <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex justify-between items-center mb-5 text-gray-500">
                     <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                        <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                        Tutorial
+                        Akademik
                     </span>
-                    <span class="text-sm">14 days ago</span>
+                    <span class="text-sm">1 days ago</span>
                 </div>
-                <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">How to quickly deploy a static website</a></h2>
-                <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
+                <img src="{{ asset('/images/Lab-Mektan.jpeg') }}" class="w-full h-56 object-cover rounded-2xl mb-3" alt="">
+                <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Website Lab Teknik Sipil</a></h2>
+                <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium pariatur praesentium tempore facere fugiat distinctio hic explicabo itaque, repellendus non ad delectus quia voluptates debitis id. Beatae neque earum provident.</p>
                 <div class="flex justify-between items-center">
                     <div class="flex items-center space-x-4">
-                        <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
+                        {{-- <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" /> --}}
                         <span class="font-medium dark:text-white">
-                            Jese Leos
+                            Admin
                         </span>
                     </div>
                     <a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
@@ -36,18 +35,18 @@
             <article class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <div class="flex justify-between items-center mb-5 text-gray-500">
                     <span class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                        <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                        Article
+                        Kegiatan
                     </span>
-                    <span class="text-sm">14 days ago</span>
+                    <span class="text-sm">1 days ago</span>
                 </div>
-                <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Our first project with React</a></h2>
-                <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
+                <img src="{{ asset('/images/Lab-Mektan.jpeg') }}" class="w-full h-56 object-cover rounded-2xl mb-3" alt="">
+                <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Test Display</a></h2>
+                <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse fugit modi, molestiae itaque, maiores exercitationem vel neque soluta quam reprehenderit beatae. Officiis explicabo asperiores facere recusandae, repellendus et quos minus?</p>
                 <div class="flex justify-between items-center">
                     <div class="flex items-center space-x-4">
-                        <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
+                        {{-- <img class="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" /> --}}
                         <span class="font-medium dark:text-white">
-                            Bonnie Green
+                            Admin
                         </span>
                     </div>
                     <a href="#" class="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
