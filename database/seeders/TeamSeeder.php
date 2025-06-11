78b2c9edcfdb381988a64d93a25dbc3493e9e03a
Erhaes <rassyahafizh@gmail.com>
2025-06-11 10:07:14 +0700

    cobaseed

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TeamSeeder extends Seeder
 {
@@ -12,6 +13,104 @@ class TeamSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('teams')->insert([
+            [
+                'nama' => 'Ir. Hery Awan Susanto., S.T.,M.T.',
+                // 'gambar' => 'heri.jpg', // Kolom gambar tidak ada di tabel teams
+                'position_id' => 1,
+                'user_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Ir. Dani Nugroho Saputro., S.Pd.T., M.Eng.',
+                // 'gambar' => 'dani.jpg',
+                'position_id' => 1,
+                'user_id' => 3,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Umigawati',
+                // 'gambar' => 'umigawati.jpg',
+                'position_id' => 2,
+                'user_id' => 4,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Ghiffara Hasna Royanisa',
+                // 'gambar' => 'ghiffara.jpg',
+                'position_id' => 2,
+                'user_id' => 5,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Ferdiono',
+                // 'gambar' => 'ferdiono.jpg',
+                'position_id' => 3,
+                'user_id' => 6,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Wildan Agus Setiadi',
+                // 'gambar' => 'wildan.jpg',
+                'position_id' => 3,
+                'user_id' => 7,
+                'created_at' => now(),    
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'M.T. Fajriyanto',
+                // 'gambar' => 'fajri.jpg',
+                'position_id' => 3,
+                'user_id' => 8,
+                'created_at' => now(),    
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Kuswanto',
+                // 'gambar' => 'kuswanto.jpg',
+                'position_id' => 3,
+                'user_id' => 9,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Amrizal Hafiz',
+                // 'gambar' => 'amrizal.jpg',
+                'position_id' => 3,
+                'user_id' => 10,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Wiwit Laras Wiranto',
+                // 'gambar' => 'wiwit.jpg',
+                'position_id' => 3,
+                'user_id' => 11,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Yulianto',
+                // 'gambar' => 'yulianto.jpg',
+                'position_id' => 3,
+                'user_id' => 12,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama' => 'Slamet',
+                // 'gambar' => 'Slamet.jpg',
+                'position_id' => 3,
+                'user_id' => 13,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            
+            ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,13 +16,111 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert(
             [
-                "name"=>"admin sipil",
-                "email"=>"laboratoriumsipil.unsoed@gmail.com",
-                "password"=>Hash::make('123456'),
-                "email_verified_at"=>null,
-                "created_at"=>now(),
-                "updated_at"=>now()
+                [
+                    "name" => "Admin Lab Sipil",
+                    "email" => "laboratoriumsipil.unsoed@gmail.com",
+                    "password" => Hash::make('123456'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Hery Awan Susanto",
+                    "email" => "hery.awan@unsoed.ac.id",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Dani Nugroho Saputro",
+                    "email" => "dani.nugroho@unsoed.ac.id",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Umigawati",
+                    "email" => "umigawati@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Ghiffara Hasna Royanisa",
+                    "email" => "ghiffara.hasna@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Ferdiono",
+                    "email" => "ferdiono@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Wildan Agus Setiadi",
+                    "email" => "wildan.agus@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "M.T. Fajriyanto",
+                    "email" => "mt.fajriyanto@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Kuswanto",
+                    "email" => "kuswanto@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Amrizal Hafiz",
+                    "email" => "amrizal.hafiz@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Wiwit Laras Wiranto",
+                    "email" => "wiwit.laras@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Yulianto",
+                    "email" => "yulianto@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
+                [
+                    "name" => "Slamet",
+                    "email" => "slamet@example.com",
+                    "password" => Hash::make('password'),
+                    "email_verified_at" => now(),
+                    "created_at" => now(),
+                    "updated_at" => now()
+                ],
             ]
-            );
+        );
     }
 }
